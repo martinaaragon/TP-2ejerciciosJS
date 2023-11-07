1ff67b43a6f39942ae9559d511a41a465b66815b
martinaaragon <144171833+martinaaragon@users.noreply.github.com>
2023-11-07 02:07:59 -0300

    Revert "Ej 6"

--- js/ejercicio-1.js
+++
@@ -1,7 +0,0 @@
-let edad= prompt("Ingrese su edad");
-
-if (edad >= 18){
-        document.write("Genial ya puedes conducir!")
-}else{
-        document.write("No tienes la edad requerida para conducir")
-}
